2e7fd2dff1a840cfb7f6a4741dd300d30401bef2
Dmitriy Dovgal <dovgal.dima.86@gmail.com>
2020-06-04 22:37:33 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+document.write('<h1>hello</h1>');
+
+/* function HDistance(strArr) { 
+
+    strArr.length;
+    return strArr; 
+  
+  }
+     strArr = ["house", "hours"];
+  // keep this function call here 
+  console.log(HDistance(strArr)); */
+  
+  function HDistance(strArr) { 
+
+    strArr.eval;
+    return strArr; 
+  
+  }
+     let s = ["house", "hours"];
+  // keep this function call here 
+  console.log(HDistance(s));
